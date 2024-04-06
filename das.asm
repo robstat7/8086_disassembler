4a6ded96e49ef6489555efe9f9ee58c06d98c4a1
Dileep Sankhla <dileepsankhla.ds@gmail.com>
2024-04-06 13:46:18 +0530

    fix comment

--- das.asm
+++ das.asm
@@ -17,7 +17,7 @@ start:
 	; write string to stdout
 	mov     rax, 1		; syscall 1 (write)
 	mov     rdi, rax	; arg 1 = 1 (stdout)
-	mov     rsi, inst	; arg 2 = msg ("AAA")
+	mov     rsi, inst	; arg 2 = msg ("AAA\n")
 	mov	rdx, 4		; arg 3 = 4 (char count)
 	syscall			; call write
 
